265a8ce5d9048cb07ae5083b6a8d089e3dc9b5db
yohanse <mehabawyohanse793@gmail.com>
2024-12-27 10:03:01 +0300

    create course module

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import USer
+from django.contrib.auth.models import User
 from django.db import models
 
 class Subject(models.Model):
@@ -8,5 +8,21 @@ class Subject(models.Model):
     class Meta:
         ordering = ['title']
 
+    def __str__(self):
+        return self.title
+    
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="courses_created")
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="courses")
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=200)
+    overview = models.TextField()
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+    
     def __str__(self):
         return self.title
