4de7dde7496e3810de598bad84f996cd2fb1cbb5
yohanse <mehabawyohanse793@gmail.com>
2024-12-27 10:29:45 +0300

    create subject admin

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -3,4 +3,5 @@ from .models import Subject, Course, Module
 
 admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
