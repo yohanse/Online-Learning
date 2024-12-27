5a46cce61f418dbd1ca89c6f080681fe25acb07b
yohanse <mehabawyohanse793@gmail.com>
2024-12-27 10:36:12 +0300

    add course admin

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -10,3 +10,11 @@ class SubjectAdmin(admin.ModelAdmin):
 class ModuleInline(admin.StackedInline):
     model = Module
 
+
+admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+   list_display = ['title', 'subject', 'created']
+   list_filter = ['created', 'subject']
+   search_fields = ['title', 'overview']
+   prepopulated_fields = {'slug': ('title',)}
+   inlines = [ModuleInline]
