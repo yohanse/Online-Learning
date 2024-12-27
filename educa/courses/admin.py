c88fcf2d37508f23a969d909c3a1bee531dc0ee6
yohanse <mehabawyohanse793@gmail.com>
2024-12-27 10:35:53 +0300

    add moudle inline

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -5,3 +5,8 @@ admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug']
     prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
