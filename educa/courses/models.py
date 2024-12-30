1cd57b9cee6cc4eb4dfb5c7722712be25c96b0ed
yohanse <mehabawyohanse793@gmail.com>
2024-12-30 14:22:21 +0300

    Implement ItemBase model

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -48,3 +48,16 @@ class Content(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="%(class)s_related")
+
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstarct = True
+
+    def __str__(self):
+        return self.title
